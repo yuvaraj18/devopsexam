f69b23bd6ceb19f0e055b915c9c4d8d8d403439f
Yuvaraj Raghottama <sryuvaraj@gmail.com>
2019-02-09 20:00:38 +0530

    New Java Project

---
+++ src/main/java/com/devops/lab/devopsexam/DevopsexamApplication.java
@@ -0,0 +1,14 @@
+package com.devops.lab.devopsexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsexamApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/devops/lab/devopsexam/DevopsexamApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devops.lab.devopsexam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsexamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
